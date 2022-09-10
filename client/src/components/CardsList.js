8aa242971e50b067c974032ba367d20208c8c1d3
Ayrow <aymeric.pilaert@hotmail.fr>
2022-09-10 16:32:36 +0200

    updated travel cards and added key prop to list items

--- client/src/components/CardsList.js
+++ client/src/components/CardsList.js
@@ -4,10 +4,10 @@ import TravelCard from './TravelCard';
 const cardsList = () => {
   return (
     <div className='py-10'>
-      <h2 className=' text-2xl text-center pb-5'>Latest experiences</h2>
+      <h2 className=' text-3xl text-center pb-5'>Latest experiences</h2>
       <div className='grid grid-cols-1 md:grid-cols-2 mx-5 gap-10 place-items-center'>
         {list.map((item) => {
-          return <TravelCard {...item} />;
+          return <TravelCard key={item.id} {...item} />;
         })}
       </div>
     </div>
--- client/src/components/TravelCard.js
+++ client/src/components/TravelCard.js
@@ -11,14 +11,14 @@ const TravelCard = ({
 }) => {
   return (
     <div
-      className=' w-full h-80 '
+      className='w-full h-80'
       style={{
         backgroundImage: `url(${image})`,
         backgroundPosition: 'center',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
       }}>
-      <div className=' w-full grid row-span-1 pt-5 bg-black h-full bg-opacity-50'>
+      <div className=' w-full grid row-span-1 pt-5 bg-black h-full bg-opacity-60'>
         <div className='grid grid-cols-2 px-5 '>
           <h3 className=' flex text-3xl'>{location}</h3>
           <div className='flex flex-col gap-2 place-items-end'>
@@ -34,7 +34,11 @@ const TravelCard = ({
           <p>Theme: {theme}</p>
           <p>{duration}</p>
         </div>
-        <div className='text-3xl text-center'>{cost} €</div>
+        <div className='text-3xl flex place-items-center justify-center'>
+          <p className='bg-orange-500 bg-opacity-70 w-1/4 text-center rounded-lg'>
+            {cost} €
+          </p>
+        </div>
       </div>
     </div>
   );
