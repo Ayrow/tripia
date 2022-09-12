99837c4b038954195416183f24068c09afcf9041
Ayrow <aymeric.pilaert@hotmail.fr>
2022-09-12 14:28:54 +0200

    setup context

---
+++ client/src/context/appContext.js
@@ -0,0 +1,24 @@
+import { createContext, useContext, useReducer } from 'react';
+import reducer from './reducer';
+
+const AppContext = createContext();
+
+const initialState = {
+  showAlert: false,
+  alertMsg: '',
+  user: null,
+};
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
+};
+
+const useAppContext = () => {
+  return useContext(AppContext);
+};
+
+export { useAppContext, AppProvider };
---
+++ client/src/context/reducer.js
@@ -0,0 +1,8 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    default:
+      throw new Error(`There is no action: ${action.type}`);
+  }
+};
+
+export default reducer;
