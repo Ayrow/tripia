8f474f61efd3df26bfe67aa40358677dc9c70b2a
Ayrow <aymeric.pilaert@hotmail.fr>
2022-09-12 11:53:35 +0200

    setup basic server and db to connect to mongodb

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
+
+//db
+import connectDB from './db/connect.js';
+
+//routers
+
+//middleware
+
+const port = process.env.PORT;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
