97a9b7f1506072b564fa3334bcd0dd538773505f
Ayrow <aymeric.pilaert@hotmail.fr>
2022-09-25 15:55:53 +0200

    setup tab in single trips to change the displayed informations

--- client/src/pages/SingleTrip.js
+++ client/src/pages/SingleTrip.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useAppContext } from '../context/appContext';
 import { FaHeart, FaArrowLeft } from 'react-icons/fa';
@@ -7,14 +7,28 @@ const SingleTrip = () => {
   let { id } = useParams();
   const navigate = useNavigate();
 
+  const initialState = {
+    summary: true,
+    activities: false,
+    costDetails: false,
+    advices: false,
+  };
+
+  const [toggleTab, setToggleTab] = useState(initialState);
+
+  const toggling = (e) => {
+    setToggleTab({ [e.target.name]: true });
+  };
+
   const { getSingleTrip, singleTrip } = useAppContext();
+
   const {
     destination,
     theme,
     duration,
     cost,
     activities,
-
+    advices,
     nbTravelers: { adults, children } = {},
   } = singleTrip;
 
@@ -62,35 +76,53 @@ const SingleTrip = () => {
         <div className='relative flex flex-col bg-white'>
           <div className=' bg-blue-700 flex justify-between p-5 text-xl'>
             <h3 className='text-2xl'>{destination}</h3>
-            <button
-              type='button'
-              className='flex items-center gap-2 btn border'>
+            <button className='flex items-center gap-2 btn border'>
               <FaHeart /> Save
             </button>
           </div>
           <div className=' bg-slate-600 flex justify-around '>
-            <button className='w-full border border-slate-200 active:bg-slate-900'>
+            <button
+              name='summary'
+              className='w-full border border-slate-200 active:bg-slate-900'
+              onClick={toggling}>
               Summary
             </button>
-            <button className='w-full border border-slate-200 active:bg-slate-900'>
+            <button
+              name='activities'
+              className='w-full border border-slate-200 active:bg-slate-900'
+              onClick={toggling}>
               Activities
             </button>
-            <button className='w-full border border-slate-200 active:bg-slate-900'>
+            <button
+              name='costDetails'
+              className='w-full border border-slate-200 active:bg-slate-900'
+              onClick={toggling}>
               Cost Details
             </button>
-            <button className='w-full border border-slate-200 active:bg-slate-900'>
-              Summary
+            <button
+              name='advices'
+              className='w-full border border-slate-200 active:bg-slate-900'
+              onClick={toggling}>
+              Advices
             </button>
           </div>
           <div className='text-black flex flex-col gap-5 p-5 text-lg'>
-            <p>Theme: {theme}</p>
-            <p>Duration: {duration} days</p>
-            <div>
-              <p>Nb of travelers:</p>
-              <p>{adults} adults</p>
-              <p>{children} children</p>
-            </div>
-            <p>Total cost: {cost}€</p>
+            {toggleTab.summary && (
+              <>
+                <p>Theme: {theme}</p>
+                <p>Duration: {duration} days</p>
+                <div>
+                  <p>Travelers:</p>
+                  <p>{adults} adults</p>
+                  <p>{children} children</p>
+                </div>
+                <p>Total cost: {cost}€</p>
+              </>
+            )}
+
+            {toggleTab.activities && <p>{activities}</p>}
+            {toggleTab.costDetails && <p>{cost}</p>}
+            {toggleTab.advices && <p>{advices}</p>}
           </div>
           <div className='absolute z-10 bottom-5 left-5'>
             <button type='button' className='btn bg-orange-600'>
