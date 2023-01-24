f0ef6bc05eb54a16915e60a51fe717e92ece168e
Ayrow <aymeric.pilaert@hotmail.fr>
2023-01-24 12:31:19 +0100

    basic display of images on singleTrip with one main and others clickable to display in main

--- client/src/components/singleTrip/GalerieImages.js
+++ client/src/components/singleTrip/GalerieImages.js
@@ -1,22 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const GalerieImages = ({ images }) => {
-  const [mainImage, setMainImage] = useState(true);
+  const [mainImage, setMainImage] = useState('');
 
   const defaultImage =
     'https://europeupclose.com/wp-content/uploads/2017/04/iceland-1751463_1280.jpg';
 
   return (
     <div className=''>
-      <img src={images[0] || defaultImage} alt='' />
-      <div className='flex gap-2  justify-between pt-4'>
+      <img
+        src={mainImage || images[0] || defaultImage}
+        alt=''
+        className=' object-contain w-full h-96'
+      />
+      <div className='flex gap-4 justify-between py-4 overflow-x-scroll scroll-auto'>
         {images.map((image, index) => {
           return (
             <img
               key={index}
-              className='w-1/5 cursor-pointer'
               src={image || defaultImage}
               alt=''
+              onClick={() => setMainImage(image)}
+              className='w-48 h-48 object-cover cursor-pointer rounded-lg'
             />
           );
         })}
