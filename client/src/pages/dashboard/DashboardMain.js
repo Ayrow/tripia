b10689c9be0e39457e9e4b4819edc9f27c9d47e3
Ayrow <aymeric.pilaert@hotmail.fr>
2023-03-16 15:19:43 +0100

    added buttons to the cards to go to relevant parts of the website

--- client/src/pages/dashboard/DashboardMain.js
+++ client/src/pages/dashboard/DashboardMain.js
@@ -1,20 +1,20 @@
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useTripContext } from '../../context/trip/tripContext';
 import { useUserContext } from '../../context/user/userContext';
 
 const DashboardMain = () => {
   const { user } = useUserContext();
   const { userTrips, getUserTrips } = useTripContext();
+  const navigate = useNavigate();
 
   useEffect(() => {
     getUserTrips();
-    console.log('user.saved', user.saved);
-    console.log('userTrips', userTrips);
   }, []);
 
   return (
-    <div className='my-5'>
-      <h2 className='text-center text-2xl mb-10 pt-4'>Dashboard</h2>
+    <div className='my-5 mx-5'>
+      <h2 className='text-center text-3xl mb-10 pt-4 font-bold'>Dashboard</h2>
       <div className='flex-wrap items-center justify-center gap-8 text-center sm:flex'>
         <div className='w-full px-4 py-4 mt-6 rounded-lg shadow-lg sm:w-1/2 md:w-1/2 lg:w-1/4 bg-gray-800'>
           <div className='flex-shrink-0'>
@@ -25,6 +25,19 @@ const DashboardMain = () => {
           <h3 className='py-4 text-2xl font-semibold sm:text-xl text-white'>
             Trips posted
           </h3>
+
+          <div className='flex gap-5 pr-5 py-5 text-xl justify-center'>
+            <button
+              className='btn btn-hipster flex items-center px-6'
+              onClick={() => navigate('/dashboard/my-trips')}>
+              See your Trips
+            </button>
+            <button
+              className='btn btn-success px-6'
+              onClick={() => navigate('/dashboard/my-trips')}>
+              Add new Trip
+            </button>
+          </div>
         </div>
 
         {/*  */}
@@ -38,6 +51,18 @@ const DashboardMain = () => {
           <h3 className='py-4 text-2xl font-semibold sm:text-xl text-white'>
             Saved Trips
           </h3>
+          <div className='flex gap-5 pr-5 py-5 text-xl justify-center'>
+            <button
+              className='btn btn-hipster flex items-center px-6'
+              onClick={() => navigate('/dashboard/saved-trips')}>
+              Go to Saved Trips
+            </button>
+            <button
+              className='btn btn-success px-6'
+              onClick={() => navigate('/explore')}>
+              Explore More trips
+            </button>
+          </div>
         </div>
 
         {/*  */}
